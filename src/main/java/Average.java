ee5f94e736047280525708906c20914d83c18a5d
Jason Zou <jasonzou@buffalo.edu>
2023-01-26 02:57:42 +0000

    completed

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,11 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        double sum = 0;
+        for (int num: nums){
+            sum += num;
+        }
+
+        return sum / nums.length;
     }
 }
